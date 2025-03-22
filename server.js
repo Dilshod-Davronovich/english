fdeb00ac8225a7df16accf3f5f1e275b6497aa89
Bobobekov Dilshod <tvbunima@gmail.com>
2025-03-22 07:38:13 +0500

    images folder added

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const pool = require('./db');
 const express = require('express');
-const multer = require('multer');
 const path = require('path');
-const cors = require('cors'); // CORS moduli
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,50 +20,10 @@ app.get('/test-db', async (req, res) => {
 // Statik fayllarni ulash (admin panel uchun)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// "audios" papkasini yaratish
-const uploadPath = path.join(__dirname, 'audios');
-
-// Multer sozlamalari
-const storage = multer.diskStorage({
-   destination: (req, file, cb) => {
-      cb(null, uploadPath); // Yuklangan fayllarni "audios" papkasiga saqlash
-   },
-   filename: (req, file, cb) => {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-      cb(null, uniqueSuffix + path.extname(file.originalname)); // Unikal nom berish
-   },
-});
-
-// Faylni faqat audio formatda yuklashni tekshirish
-const fileFilter = (req, file, cb) => {
-   if (file.mimetype.startsWith('audio/')) {
-      cb(null, true);
-   } else {
-      cb(new Error('Faqat audio fayllarni yuklash mumkin!'), false);
-   }
-};
-
-const upload = multer({ storage, fileFilter });
-
-// Audio fayl yuklash uchun endpoint
-app.post('/upload', upload.single('audioFile'), (req, res) => {
-   if (!req.file) {
-      return res.status(400).json({ message: 'Hech qanday audio yuklanmadi!' });
-   }
-   res.json({
-      message: 'Audio muvaffaqiyatli yuklandi!',
-      file: req.file.filename,
-   });
-});
-
-// Admin panelni ochish uchun asosiy sahifa
 app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// "audios" papkasidagi fayllarga to‘g‘ridan-to‘g‘ri kirish imkoniyati
-app.use('/audios', express.static(uploadPath));
-
 app.listen(PORT, () => {
-   console.log(`Server http://localhost:${PORT} da ishga tushdi`);
+   console.log(`Server :${PORT} da ishga tushdi`);
 });
